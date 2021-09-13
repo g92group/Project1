9d441fc21b5b43a9effa9b9c8e13de361409923b
AbdulazizSulaymon <djshahrick@gmail.com>
2021-09-13 21:13:56 +0500

    page2

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.css';
 
-
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
